690de52d99a01330efee0316041366a75e041712
Ole Mathias Aa. Heggem <ole.mathias.aa.heggem@basefarm.com>
2020-11-24 18:19:39 +0100

    Use connected-react-router

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-
-import store from './store'
+import { ConnectedRouter } from 'connected-react-router'
+import configureStore, { history } from './store'
 import { Provider } from 'react-redux'
+
+const store = configureStore()
 
 ReactDOM.render(<React.StrictMode>
   <Provider store={store}>
-    <App/>
+    <ConnectedRouter history={history}>
+        <App/>
+    </ConnectedRouter>
   </Provider>
 </React.StrictMode>, document.getElementById('root'))
