dcec9d598691cb5b894f780f7e5a562eb2b3cc4b
Ole Mathias Aa. Heggem <ole.mathias.aa.heggem@basefarm.com>
2020-11-25 11:54:26 +0100

    bug: fix mobile navbar

--- src/components/Base/sidebar.jsx
+++ src/components/Base/sidebar.jsx
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import * as Config from '../../constants/config'
 
+import Navbar from 'react-bootstrap/Navbar'
+
 function SideBar () {
   const user = useSelector(state => state.user)
   let userNavBar
@@ -26,21 +28,28 @@ function SideBar () {
         <li className="nav-item">
           <a className="nav-link" href={Config.API_URL}>Rest API</a>
         </li>
+        <li className="nav-item">
+          <Link className="nav-link d-md-none" to="/logout">Sign out</Link>
+        </li>
       </div>
     )
   }
 
   return (
-    <nav id="sidebarMenu" className="col-md-3 col-lg-2 d-md-block bg-dark sidebar collapse">
-      <div className="sidebar-sticky pt-3">
-        <ul className="nav flex-column">
-          <li className="nav-item">
-            <Link className="nav-link" to="/">Home</Link>
-          </li>
-          {userNavBar}
-        </ul>
-      </div>
-    </nav>
+    <Navbar expand="md">
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" className="d-md-none" />
+      <Navbar.Collapse id="responsive-navbar-nav" className="col-md-3 col-lg-2 d-md-block bg-dark sidebar">
+        <div className="sidebar-sticky pt-3">
+          <ul className="nav flex-column">
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" className="d-md-none" />
+            <li className="nav-item">
+              <Link className="nav-link" to="/">Home</Link>
+            </li>
+            {userNavBar}
+          </ul>
+        </div>
+      </Navbar.Collapse>
+    </Navbar>
   )
 }
 
--- src/components/Base/topbar.jsx
+++ src/components/Base/topbar.jsx
@@ -8,7 +8,7 @@ function TopBar () {
   let userBar
 
   if (user.logged_in) {
-    userBar = (<ul className="navbar-nav px-3">
+    userBar = (<ul className="navbar-nav px-3 d-none d-md-block">
         <li className="nav-item text-nowrap">
           <Link className="nav-link" to="/logout">Sign out</Link>
         </li>
@@ -20,9 +20,6 @@ function TopBar () {
     <nav className="navbar navbar-dark sticky-top bg-primary flex-md-nowrap p-0 shadow">
       <Link className="navbar-brand col-md-3 col-lg-2 mr-0 px-3" to="/">Orc <small>ALPHA</small>
       </Link>
-      <button className="navbar-toggler position-absolute d-md-none collapsed" type="button" data-toggle="collapse" data-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
       {userBar}
     </nav>)
 }
